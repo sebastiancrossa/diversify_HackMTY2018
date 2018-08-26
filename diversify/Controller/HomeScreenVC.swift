9d33ff148f674a4021ed2419370a0e27d8c91fd3
Sebastian C <crossasebastian@gmail.com>
2018-08-26 01:29:06 -0500

    Basic UI added

---
+++ diversify/Controller/HomeScreenVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeScreenVC.swift
+//  diversify
+//
+//  Created by Sebastian Crossa on 8/26/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
