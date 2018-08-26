73ff454c24bec0349e284a09835e568a0b945fd3
Sebastian C <crossasebastian@gmail.com>
2018-08-26 05:49:00 -0500

    Complete rehaul of UI

--- diversify/Controller/HomeScreenVC.swift
+++ diversify/Controller/HomeScreenVC.swift
@@ -9,50 +9,15 @@
 import UIKit
 
 class HomeScreenVC: UIViewController {
-    
-    @IBOutlet weak var experienceCollectionView: UICollectionView!
-    
-    var experiences = Experience.fetchExperiences()
-    let cellScaling: CGFloat = 6.0
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        let screenSize = UIScreen.main.bounds.size
-        let cellWidth = CGFloat(319)
-        let cellHeight = CGFloat(255)
         
-        let insetX = (view.bounds.width - cellWidth)
-        let insetY = (view.bounds.height - cellHeight)
-        
-        let layout = experienceCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
-        experienceCollectionView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
-        */
-        
-        experienceCollectionView?.dataSource = self
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-}
-
-extension HomeScreenVC: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return experiences.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = experienceCollectionView.dequeueReusableCell(withReuseIdentifier: "experienceCell", for: indexPath) as! ExperienceCell
-        
-        cell.experience = experiences[indexPath.item]
-        return cell
-    }
 }
--- diversify/View/ExperienceCell.swift
+++ diversify/View/ExperienceCell.swift
@@ -9,39 +9,6 @@
 import UIKit
 
 class ExperienceCell: UICollectionViewCell {
-    
-    @IBOutlet weak var experienceImage: UIImageView!
-    @IBOutlet weak var experienceTitle: UILabel!
-    @IBOutlet weak var experienceDescription: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
-    
-    var experience: Experience? {
-        didSet {
-            self.updateUI()
-        }
-    }
-    
-    private func updateUI() {
-        if let experience = experience {
-            experienceImage.image = UIImage(named: "experiencia1")
-            experienceTitle.text = "Preparar tacos en una carreta"
-            experienceDescription.text = "Tienes la oportunidad de vivir una de las expriencias más cercanas a México posible, junto con un completo profesional de tacos."
-        } else {
-            experienceImage.image = nil
-            experienceTitle.text = nil
-            experienceDescription.text = nil
-        }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        self.layer.cornerRadius = 3.0
-        self.clipsToBounds = true
-    }
-    
-    @IBAction func contratarButtonWasPressed(_ sender: Any) {
-        
-    }
+
     
 }
