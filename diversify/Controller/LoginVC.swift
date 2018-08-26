79de905fbfe558c1c6cee5815e39f25fb7e372e3
Sebastian C <crossasebastian@gmail.com>
2018-08-26 01:17:28 -0500

    Configured with Firebase

---
+++ diversify/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  diversify
+//
+//  Created by Sebastian Crossa on 8/25/18.
+//  Copyright © 2018 Sebastian Crossa. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
